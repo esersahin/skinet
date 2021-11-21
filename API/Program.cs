770b744b3c1b9a9106948ff11c4cb6fa3e6b45cf
e.sahin <e.sahin@amatis.nl>
2021-11-21 12:35:15 +0300

    Applying migration,creating database add seed data

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Infrastructure.Data;
 using Core.Interfaces;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +13,21 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<StoreContext>(x =>
-    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(connectionString));
+
+//builder.Services.AddScoped( _ => new SqliteConnection(connectionString));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//var logger = app.Services.GetRequiredService<ILoggerFactory>();
+
+await EnsureDb(app.Services, app.Logger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -35,3 +43,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{ 
+    logger.LogInformation("Ensuring database exists at connection string '[connectionString]'", connectionString);
+ 
+    try
+    {
+         var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<StoreContext>();
+         await context.Database.MigrateAsync();
+         await StoreContextSeed.SeedAsync(context, logger);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "An error occurred during migration");
+    }
+}
